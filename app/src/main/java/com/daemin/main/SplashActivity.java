dae75912d9cd3b729910e3296e3944500f31d8ad
hernia <hernia@koreatech.ac.kr>
2015-12-21 18:45:42 +0900

    업데이트버전체크

--- app/src/main/java/com/daemin/main/SplashActivity.java
+++ app/src/main/java/com/daemin/main/SplashActivity.java
@@ -5,7 +5,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
@@ -17,20 +16,10 @@ import android.util.DisplayMetrics;
 import android.util.Log;
 import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.daemin.common.CustomJSONObjectRequest;
-import com.daemin.common.GPSInfo;
 import com.daemin.common.MyRequest;
-import com.daemin.common.MyVolley;
 import com.daemin.encryption.MyHash;
 import com.daemin.enumclass.User;
 import com.daemin.timetable.R;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
@@ -57,17 +46,6 @@ public class SplashActivity extends Activity{
 		MyRequest.getGroupList(this);
 	}
 	private void initialize() {
-		/*if(Common.isOnline()) {
-			if (User.USER.isGroupListDownloadState()) {
-				Toast.makeText(this, "그룹리스트다운로드되있는상태", Toast.LENGTH_SHORT).show();
-			}else{
-				Toast.makeText(this, "그룹리스트다운로드해야함", Toast.LENGTH_SHORT).show();
-				//MyRequest.getGroupList();
-			}
-		}
-		else{
-			Toast.makeText(this, this.getString(R.string.network_error), Toast.LENGTH_SHORT).show();
-		}*/
 		Log.i("userPK",User.INFO.getUserPK());
 		if(User.INFO.getFirstFlag()) {
 			Toast.makeText(
@@ -76,15 +54,15 @@ public class SplashActivity extends Activity{
 					Toast.LENGTH_LONG).show();
 			firstSetting();
 		}
-		GPSInfo gps = new GPSInfo(this);
+		/*GPSInfo gps = new GPSInfo(this);
 		// GPS 사용유무 가져오기
 		if (gps.isGetLocation()) {
 			User.INFO.setLatitude(gps.getLatitude());
 			User.INFO.setLongitude(gps.getLongitude());
-			/*Toast.makeText(
+			*//*Toast.makeText(
 					this,
 					"내 위치 - \n위도: " + gps.getLatitude() + "\n경도: " + gps.getLongitude(),
-					Toast.LENGTH_LONG).show();*/
+					Toast.LENGTH_LONG).show();*//*
 			gps.stopUsingGPS();
 		} else {
 			// GPS 를 사용할수 없으므로
@@ -93,7 +71,7 @@ public class SplashActivity extends Activity{
 					"현재 GPS가 켜져있지 않습니다.",
 					Toast.LENGTH_LONG).show();
 			gps.stopUsingGPS();
-		}
+		}*/
 
 		//MyRequest.test(singleton);
 		Handler handler = new Handler() {
